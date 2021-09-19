a828e4a8c54e4da4afab5752d69997bdfc9f2f66
alefnull <nullalef@gmail.com>
2021-09-19 12:43:47 -0400

    draw->tick, 69 lines (nice)

--- stars.js
+++ stars.js
@@ -2,6 +2,7 @@ const cnvs = document.getElementById("cnvs");
 const ctx = cnvs.getContext("2d");
 cnvs.width = window.innerWidth;
 cnvs.height = window.innerHeight;
+
 class Star {
   constructor(x, y, z, color) {
     this.x = x;
@@ -9,17 +10,20 @@ class Star {
     this.z = z;
     this.color = color;
   }
+
   update(speed) {
     this.x -= speed;
     if (this.x < 0) {
       this.x = cnvs.width;
     }
   }
+
   draw() {
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x, this.y, this.z, this.z);
   }
 }
+
 class Starfield {
   constructor(numStars, speed, color) {
     this.stars = [];
@@ -28,17 +32,20 @@ class Starfield {
       this.stars.push(new Star(Math.random() * cnvs.width, Math.random() * cnvs.height, Math.random() * 2, color));
     }
   }
+
   update() {
     for (let i = 0; i < this.stars.length; i++) {
       this.stars[i].update(this.speed);
     }
   }
+
   draw() {
     for (let i = 0; i < this.stars.length; i++) {
       this.stars[i].draw();
     }
   }
 }
+
 const starfields = [];
 for (let i = 0; i < 3; i++) {
   let numStars = Math.floor(Math.random() * 100) + 50;
@@ -46,16 +53,17 @@ for (let i = 0; i < 3; i++) {
   let r = Math.floor(Math.random() * 255);
   let g = Math.floor(Math.random() * 255);
   let b = Math.floor(Math.random() * 255);
-  let color = `rgb(${r},${g},${b})`;
-  starfields.push(new Starfield(numStars, speed, color));
+  starfields.push(new Starfield(numStars, speed, `rgb(${r},${g},${b})`));
 }
-function draw() {
+
+function tick() {
   ctx.fillStyle = "rgba(0,0,0,0.3)";
   ctx.fillRect(0, 0, cnvs.width, cnvs.height);
   for (let i = 0; i < starfields.length; i++) {
     starfields[i].update();
     starfields[i].draw();
   }
-  requestAnimationFrame(draw);
+  requestAnimationFrame(tick);
 }
-draw();
+
+tick();
